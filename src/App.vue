bcbbb2125b43595ec89f4448df96c5c93f075a03
Richardson William <richardsonwtr@gmail.com>
2019-11-08 01:36:44 -0300

    Showing question box and navigating through the questions

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col md="6" offset-md="3">
+          <QuestionBox v-if="questions.length" :currentQuestion="questions[index]" :next="next" />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    }
+  },
+  mounted: function() {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then(res => res.json())
+      .then(data => {
+        if (data.response_code != 0) return;
+        this.questions = data.results;
+      });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <b-nav tabs>
+        <b-nav-item disabled>
+          <b>Fancy Quiz App</b>
+        </b-nav-item>
+        <b-nav-item disabled>Counter: 4/10</b-nav-item>
+      </b-nav>
+    </div>
+  </div>
+</template>
