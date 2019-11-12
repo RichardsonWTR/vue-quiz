ae4fbe34fe6d2117e619ff9750f2251a65dff1cf
Richardson William <richardsonwtr@gmail.com>
2019-11-12 06:20:42 -0300

    Adding home message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Home</h1>
+  <div id="app">
+    <h1>Welcome to my <i>Fancy Quiz App</i></h1>
+    <h3>
+      Test your knowledge among many different categories!
+    </h3>
+    <p>
+      Share with your friends!
+    </p>
+  </div>
 </template>
 <script>
 export default {
