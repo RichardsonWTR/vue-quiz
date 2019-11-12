c261c4b42a70a326d2ef793258d0460eb31e099d
Richardson William <richardsonwtr@gmail.com>
2019-11-12 04:29:55 -0300

    Changing dist files

---
+++ dist/precache-manifest.5274b5dea812e9f26017cff284587d19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98de9c0d57991c6f240",
+    "url": "/vue-quiz/css/app.e1b54816.css"
+  },
+  {
+    "revision": "4d0d41129752b3a66e65",
+    "url": "/vue-quiz/css/chunk-vendors.0d0a218f.css"
+  },
+  {
+    "revision": "ef87b1e7d725d9863d652168f00e1a2f",
+    "url": "/vue-quiz/index.html"
+  },
+  {
+    "revision": "d98de9c0d57991c6f240",
+    "url": "/vue-quiz/js/app.78566595.js"
+  },
+  {
+    "revision": "4d0d41129752b3a66e65",
+    "url": "/vue-quiz/js/chunk-vendors.3c83c166.js"
+  },
+  {
+    "revision": "4d7f7bbf1982510f4e1bb8ede3d8f30d",
+    "url": "/vue-quiz/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-quiz/robots.txt"
+  }
+]);
